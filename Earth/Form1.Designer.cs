8e0481a48cbfc60bf1199ef6ec6e61877269e6c1
goingzero.tool <goingzero.tool@gmail.com>
2024-05-03 18:07:51 +0900

    sub message

--- Earth/Form1.Designer.cs
+++ Earth/Form1.Designer.cs
@@ -20,26 +20,27 @@
 			base.Dispose(disposing);
 		}
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		///  Required method for Designer support - do not modify
-		///  the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			SuspendLayout();
-			// 
-			// Form1
-			// 
-			AutoScaleDimensions = new SizeF(7F, 15F);
-			AutoScaleMode = AutoScaleMode.Font;
-			ClientSize = new Size(340, 170);
-			Name = "Form1";
-			Text = "Form1";
-			ResumeLayout(false);
-		}
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(340, 170);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
 
-		#endregion
-	}
+        #endregion
+    }
 }
